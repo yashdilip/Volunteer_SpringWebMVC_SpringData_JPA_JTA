5c526cc1075fd366e0f40dcee99f4226bc6e07df
dilip <er.dilip.n@gmail.com>
2016-08-21 14:00:42 -0500

    startup project

---
+++ volunteering/src/main/java/cs544/edu/mum/volunteering/App.java
@@ -0,0 +1,13 @@
+package cs544.edu.mum.volunteering;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
